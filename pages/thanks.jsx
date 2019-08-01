c068daea7912bddc7c5625413a851bedc878aae2
bartwr <mail@bartroorda.nl>
2019-08-01 23:07:40 +0200

    :sparkles: Added /thanks page
    Fixes #3

---
+++ pages/thanks.jsx
@@ -0,0 +1,38 @@
+import dynamic from 'next/dynamic';
+import Head from 'next/head';
+import Link from 'next/link';
+
+// Load components
+const Navigation = dynamic(() => import('../components/navigation.jsx'));
+const PageHeader = dynamic(() => import('../components/page-header.js'));
+const Title = dynamic(() => import('../components/title.js'));
+const Footer = dynamic(() => import('../components/footer.js'));
+
+function Thanks() {
+  return <div className="root">
+    <Head>
+      <title>Privacy policy - The Hague Tech</title>
+    </Head>
+
+    <Navigation />
+
+    <div className="content-wrapper">
+      <Title size="large">
+        Thanks you!
+      </Title>
+    </div>
+
+    <Footer />
+
+    <style jsx global>{`
+      .content-wrapper {
+        padding: 140px 12px;
+        width: 800px;
+        max-width: 100%;
+        margin: 0 auto;
+      }
+    `}</style>
+  </div>
+}
+
+export default Thanks;

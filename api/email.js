6690caa74d52bd564503f9efc1b904736c414851
bartwr <mail@bartroorda.nl>
2019-11-12 10:21:23 +0100

    :bug: Send Contact form emails from @thehaguetech.com
    fixes #13

--- api/email.js
+++ api/email.js
@@ -4,9 +4,10 @@ const sendMail = function(from) {
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
   const msg = {
     to: 'launch@thehaguetech.com',
-    from: `${from.name} <${from.email}>`,
+    from: 'website@thehaguetech.com',
     subject: 'Email from thehaguetech.nl',
-    html: from.message.replace("\n", '<br/>') + "<br /><br />"
+    html: `${from.name} <${from.email}> sent the following message:<br /><hr /><br />` +
+          from.message.replace("\n", '<br/>') + "<br /><br />"
   };
   sgMail.send(msg);
 }

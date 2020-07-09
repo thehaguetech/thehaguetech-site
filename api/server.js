f65da8a11fd4a168db4b86b87b46f59ccaf5dbb0
davidmesman <david@uptimemonkeys.com>
2020-07-09 12:52:43 +0200

    (server.js): Added dotenv to fix error with env variables

--- api/server.js
+++ api/server.js
@@ -2,11 +2,14 @@
 // This file doesn't go through babel or webpack transformation.
 // Make sure the syntax and sources this file requires are compatible with the current node version you are running
 // See https://github.com/zeit/next.js/issues/1245 for discussions on Universal Webpack or universal Babel
+
+require('dotenv').config();
+
 const { createServer } = require('http');
 const { parse } = require('url');
 const { createReadStream } = require('fs');
 const sm = require('sitemap');
-const express = require('express')
+const express = require('express');
 const next = require('next');
 
 const dev = process.env.NODE_ENV !== 'production';
